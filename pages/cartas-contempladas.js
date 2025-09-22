e326cff58a6cdf3fc213ab562ff67d68bb31dbdf
Diegofreimuth <diego@freimuth.com.br>
2025-09-22 18:19:43 -0300

    Create cartas-contempladas.js

---
+++ pages/cartas-contempladas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CartasContempladas() {
+  return (
+    <iframe 
+      src="https://www.casagauchaconsorcios.com.br/contemplados" 
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  );
+}
