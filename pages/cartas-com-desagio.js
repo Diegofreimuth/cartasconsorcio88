62aa3f85352e8b662a83bbc61949895204c9287b
Diegofreimuth <diego@freimuth.com.br>
2025-09-22 18:20:34 -0300

    Create cartas-com-desagio.js

---
+++ pages/cartas-com-desagio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CartasComDesagio() {
+  return (
+    <iframe 
+      src="https://www.casagauchaconsorcios.com.br/desagio" 
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  );
+}
